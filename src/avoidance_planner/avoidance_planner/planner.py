1ed6c96637266c5a01c71266151cd36058781506
Weathereds <47501569+Weathereds@users.noreply.github.com>
2022-12-03 14:46:37 -0500

    Update planner.py

--- src/avoidance_planner/avoidance_planner/planner.py
+++ src/avoidance_planner/avoidance_planner/planner.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from avoidance_planner.trajectory.functions import rrt_star
+from avoidance_planner.trajectory.trajectory import *
 from avoid_plan_msgs.srv import MakePlan
 
 
@@ -18,19 +18,34 @@ class PlannerNode(Node):
         # request contians the path, response should contain the result at the end
 
         # copy the frame ID
-        response.path_out.header = request.path_in.header 
+        response.path_out.header = request.path_in.header
 
-        # do the planning bit
+        # Define the start and end XYZ coordinates from the path_in request
+        start_coords = request.path_in.poses[0].pose.point
+        end_coords = request.path_in.poses[-1].pose.point
 
-        # fill out the Z height
+        # Calculate the XYZ coordinates of the path from start to end coordinates
+        trajectory = generate_trajectory(start_coords, end_coords)
+
+        # Define empty poses list
+        poses = []
+
+        # For each set of coordinates in the trajectory
+        for point in trajectory:
+            # Define the stamped pose structure and insert the XYZ point and original orientation
+            ps = PoseStamped()
+            ps.pose.point.x = point[0]
+            ps.pose.point.y = point[1]
+            ps.pose.point.z = point[2]
+            ps.pose.orientation = start_coords.pose.orientation
+            poses.append(ps)
 
         # push the data into the message
-
+        response.path_out.poses = poses
 
         return response
 
-
-
+    
 def main():
     # starts and runs the ros node class above
     rclpy.init()
